525d31d759c9e7d822417af3ba6a1f0320b277a6
Josh <jshwa86@gmail.com>
2014-09-08 19:36:56 -0500

    Added a new utility class that provides the ability to identify a droplet based on name.
    Created a new DropletScanner class that offers a "findDropletId"
    function, which is either going to be extremely useful -or- totally
    useless.

--- src/service/main.js
+++ src/service/main.js
@@ -6,7 +6,56 @@ var Express = require('express'),
     q = require('q'),
     SECURITY_TOKEN = process.env.DO_TOKEN,
     SERVER_ID = process.env.DO_SERVER_ID,
+    SERVER_NAME = process.env.DO_SERVER_NAME,
     SERVICE_BASE_URL = "https://api.digitalocean.com/v2";
+
+/**
+ *
+ * Droplet Scanner
+ * DO's API states that [the droplet ID] is automatically generated upon Droplet creation.
+ * This object provides an easy way to retrieve the dropletID, given the droplet name,
+ * provided that the droplet exists.
+ *
+ */
+
+function DropletScanner(){
+        
+        var url = SERVICE_BASE_URL + "/droplets",
+        listRequest = {
+            "Authorization": "Bearer " + SECURITY_TOKEN,
+            "Content-Type": "application/json"
+        };
+        /**
+         * Promise-based function that returns an object with the following format:
+         * { 
+         *    "dropletFound":true|false, //True if we found the dropletID
+         *    "dropletId":1234567        //The droplet ID that was found. Only populated if dropletFound was true.
+         * }    
+         *
+         */
+        this.findDropletId = function (nameOfDropletToFind){
+            var deferred = q.defer(),
+            result ={
+                        "dropletFound":false
+                    };
+            http.request(url, function (data, err) {
+                if (!err) {
+                    var droplets = JSON.parse(data).droplets;
+                    for (var i = 0; i < droplets.length; i++) {
+                        if(droplets[i].name === nameOfDropletToFind){
+                            result.dropletFound = true;
+                            result.dropletId = droplets[i].id;
+                        }
+                    }
+                    deferred.resolve(result);
+                
+                } else {
+                   deferred.reject(err);
+                }
+            }, null, listRequest);
+            return deferred.promise;
+        }
+}
 
 /**
  *
@@ -68,7 +117,8 @@ function main() {
     "use strict";
     var app = new Express(),
         droplet = new Droplet(SERVER_ID),
-        pServerState = "unknown";
+        pServerState = "unknown",
+        dropletScanner = new DropletScanner();
 
     function throwError(response, message) {
         console.log("Error message: " + message);
@@ -97,6 +147,16 @@ function main() {
         }, function (err) {
             throwError(response, err);
         });
+    });
+    
+    app.get('/identify', function (request, response) {
+       
+        dropletScanner.findDropletId(SERVER_NAME).then(function (result) {
+            response.set('Content-Type', 'text/plain').send(result);
+        }, function (err) {
+            throwError(response, err);
+        });
+        
     });
 
     app.listen(process.env.PORT || 8080);
